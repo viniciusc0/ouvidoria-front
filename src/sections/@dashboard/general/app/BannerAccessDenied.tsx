0082df27e380c90e13eff6da74e42bdb2999ad0f
Vinicius Oliveira <vinicius.oliveira@Desktop-00293423.local>
2023-05-22 23:26:34 -0300

    feat: implementação do apollo form

--- src/sections/@dashboard/general/app/BannerAccessDenied.tsx
+++ src/sections/@dashboard/general/app/BannerAccessDenied.tsx
@@ -1,11 +1,10 @@
+import { Box, Button, Container, Typography } from '@mui/material'
 import { styled } from '@mui/material/styles'
-import { Typography, Button, Container, Box } from '@mui/material'
-import { PageNotFoundIllustration } from '../../../../assets'
 import { m } from 'framer-motion'
-import { MotionContainer, varBounce } from '../../../../components/animate'
 import NextLink from 'next/link'
 import Iconify from '../../../../components/Iconify'
-import Image from '../../../../components/Image'
+import { MotionContainer, varBounce } from '../../../../components/animate'
+import Image from '../../../../components/image/'
 
 const RootStyle = styled('div')(({ theme }) => ({
     display: 'flex',
@@ -20,33 +19,20 @@ interface BannerAccessDeniedProps {
     description: string
 }
 
-export default function BannerAccessDenied({
-    title,
-    description,
-}: BannerAccessDeniedProps) {
+export default function BannerAccessDenied({ title, description }: BannerAccessDeniedProps) {
     return (
         <RootStyle>
             <Container component={MotionContainer}>
-                <Box
-                    sx={{ maxWidth: 480, margin: 'auto', textAlign: 'center' }}
-                >
+                <Box sx={{ maxWidth: 480, margin: 'auto', textAlign: 'center' }}>
                     <m.div variants={varBounce().in}>
                         <Typography variant="h3" paragraph>
                             {title}
                         </Typography>
                     </m.div>
-                    {description && (
-                        <Typography sx={{ color: 'text.secondary' }}>
-                            {description}
-                        </Typography>
-                    )}
+                    {description && <Typography sx={{ color: 'text.secondary' }}>{description}</Typography>}
 
                     <m.div variants={varBounce().in}>
-                        <Image
-                            sx={{ height: 360, my: { xs: 5, sm: 10 } }}
-                            disabledEffect
-                            src={'/images/erro403.svg'}
-                        />
+                        <Image sx={{ height: 360, my: { xs: 5, sm: 10 } }} disabledEffect src={'/images/erro403.svg'} />
                     </m.div>
                     <NextLink href="/app/home" passHref>
                         <Button size="large" variant="contained">
