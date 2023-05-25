05f900dda68091915cee64a7ca546424145f3892
Caio <caiossfer2@gmail.com>
2023-05-25 14:26:35 -0300

    fix: centralização do texto do modal formulario

--- src/components/ouvidoria/FormSuccess/index.tsx
+++ src/components/ouvidoria/FormSuccess/index.tsx
@@ -4,17 +4,17 @@ import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline'
 
 function FormSuccess({ protocol }: { protocol: string }) {
     return (
-        <Grid padding="20px">
-            <Grid display="flex" justifyContent="center" marginY="10px">
+        <Grid padding="20px" paddingBottom="0">
+            <Grid display="flex" justifyContent="center" marginTop="10px">
                 <CheckCircleOutlineIcon sx={{ fontSize: '60px', color: 'green' }} />
             </Grid>
-            <Typography variant="h4" fontWeight="normal" color="green" marginBottom="10px">
+            <Typography variant="h4" fontWeight="normal" color="green" marginBottom="10px" textAlign="center">
                 Formulário enviado com sucesso!
             </Typography>
-            <Typography variant="h6" fontWeight="normal" color="#474747">
+            <Typography variant="h6" fontWeight="normal" color="#474747" textAlign="center">
                 O número do protocolo é:
             </Typography>
-            <Typography variant="h6" fontWeight="bold">
+            <Typography variant="h6" fontWeight="bold" textAlign="center">
                 {protocol}
             </Typography>
         </Grid>
